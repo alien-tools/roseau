04b7698553b0b015f69d7c32b405e7ebe1e62a5f
Thomas Degueule <thomas.degueule@labri.fr>
2023-09-11 14:56:54 +0200

    main: pick the appropriate Launcher kind

--- src/main/java/com/github/maracas/roseau/roseau.java
+++ src/main/java/com/github/maracas/roseau/roseau.java
@@ -4,6 +4,7 @@ import com.github.maracas.roseau.changes.BreakingChange;
 import spoon.Launcher;
 import spoon.MavenLauncher;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import java.util.List;
@@ -25,13 +26,11 @@ public class roseau {
             String path2 = args[1];
 
             Path v1 = Path.of(path1);
-            Launcher launcher1 = new MavenLauncher(v1.toString(), MavenLauncher.SOURCE_TYPE.APP_SOURCE, new String[0]);
-            launcher1.getEnvironment().setNoClasspath(true);
+            Launcher launcher1 = launcherFor(v1);
             APIExtractor extractor1 = new APIExtractor(launcher1.buildModel());
 
             Path v2 = Path.of(path2);
-            Launcher launcher2 = new MavenLauncher(v2.toString(), MavenLauncher.SOURCE_TYPE.APP_SOURCE, new String[0]);
-            launcher2.getEnvironment().setNoClasspath(true);
+            Launcher launcher2 = launcherFor(v2);
             APIExtractor extractor2 = new APIExtractor(launcher2.buildModel());
 
             APIDiff diff = new APIDiff(extractor1.extractingAPI(), extractor2.extractingAPI());
@@ -44,6 +43,18 @@ public class roseau {
         } catch (spoon.SpoonException e) {
             System.err.println(" Please provide two valid paths ");
 
+        }
+    }
+
+    public static Launcher launcherFor(Path location) {
+        if (Files.exists(location.resolve("pom.xml")))
+            return new MavenLauncher(location.toString(), MavenLauncher.SOURCE_TYPE.APP_SOURCE, new String[0]);
+        else {
+            Launcher launcher = new Launcher();
+            launcher.getEnvironment().setComplianceLevel(11);
+            launcher.getEnvironment().setNoClasspath(true);
+            launcher.addInputResource(location.toString());
+            return launcher;
         }
     }
 
