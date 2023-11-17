c66d1056c1c987cab87198039f50a60e1b4ea0f0
Thomas Degueule <thomas.degueule@labri.fr>
2023-11-17 22:39:39 +0100

    Basic test infrastructure

--- src/test/java/com/github/maracas/roseau/diff/ClassRemovedTest.java
+++ src/test/java/com/github/maracas/roseau/diff/ClassRemovedTest.java
@@ -1,71 +1,98 @@
 package com.github.maracas.roseau.diff;
 
-import com.github.maracas.roseau.api.APIExtractor;
-import com.github.maracas.roseau.api.SpoonAPIExtractor;
-import com.github.maracas.roseau.diff.APIDiff;
-import com.github.maracas.roseau.diff.changes.BreakingChange;
-import com.github.maracas.roseau.diff.changes.BreakingChangeKind;
 import org.junit.jupiter.api.Test;
-import org.opentest4j.AssertionFailedError;
-import spoon.Launcher;
-import spoon.reflect.CtModel;
-import spoon.support.compiler.VirtualFile;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.github.maracas.roseau.diff.changes.BreakingChangeKind.*;
+import static com.github.maracas.roseau.diff.DiffTestUtils.assertBC;
+import static com.github.maracas.roseau.diff.DiffTestUtils.assertNoBC;
+import static com.github.maracas.roseau.diff.DiffTestUtils.buildDiff;
+import static com.github.maracas.roseau.diff.changes.BreakingChangeKind.CLASS_REMOVED;
 
 class ClassRemovedTest {
 	@Test
-	void foo() {
+	void class_private_removed() {
 		String v1 = "class A {}";
 		String v2 = "class B {}";
 
-		var bcs = buildDiff(v1, v2);
-		assertBC("A", CLASS_REMOVED, 1, bcs);
+		assertNoBC(buildDiff(v1, v2));
 	}
 
-	static void assertBC(String symbol, BreakingChangeKind kind, int line , List<BreakingChange> bcs) {
-		List<BreakingChange> matches = bcs.stream()
-			.filter(bc ->
-				   kind   == bc.kind()
-				&& line   == bc.location().beginLine()
-				&& symbol.equals(bc.impactedSymbol().getQualifiedName())
-			)
-			.toList();
+	@Test
+	void class_public_removed() {
+		String v1 = "public class A {}";
+		String v2 = "public class B {}";
 
-		String desc = "[%s, %s, %d]".formatted(symbol, kind, line);
-
-		if (matches.isEmpty()) {
-			String found = bcs.stream()
-				.filter(bc -> bc.location().beginLine() == line)
-				.map(bc -> "[%s, %s, %d]".formatted(bc.impactedSymbol().getQualifiedName(), bc.kind(), bc.location().beginLine()))
-				.collect(Collectors.joining(", "));
-			throw new AssertionFailedError("No match", desc, found);
-		}
-
-		if (matches.size() > 1) {
-			String found = matches.stream()
-				.map(bc -> "[%s, %s, %d]".formatted(bc.impactedSymbol().getQualifiedName(), bc.kind(), bc.location().beginLine()))
-				.collect(Collectors.joining(", "));
-
-			throw new AssertionFailedError("Multiple matches", desc, found);
-		}
+		assertBC("A", CLASS_REMOVED, 1, buildDiff(v1, v2));
 	}
 
-	private List<BreakingChange> buildDiff(String sourcesV1, String sourcesV2) {
-		APIExtractor extractor1 = new SpoonAPIExtractor(buildModel(sourcesV1));
-		APIExtractor extractor2 = new SpoonAPIExtractor(buildModel(sourcesV2));
-		return new APIDiff(extractor1.extractAPI(), extractor2.extractAPI()).diff();
+	@Test
+	void class_public_kept() {
+		String v1 = "public class A {}";
+		String v2 = "public class A {}";
+
+		assertNoBC(buildDiff(v1, v2));
 	}
 
-	private CtModel buildModel(String sources) {
-		Launcher launcher = new Launcher();
+	@Test
+	void class_public_moved() {
+		String v1 = "package a; public class A {}";
+		String v2 = "package b; public class A {}";
 
-		launcher.addInputResource(new VirtualFile(sources));
-		launcher.getEnvironment().setComplianceLevel(17);
+		assertBC("a.A", CLASS_REMOVED, 1, buildDiff(v1, v2));
+	}
 
-		return launcher.buildModel();
+	@Test
+	void class_inner_private_in_class_public_removed() {
+		String v1 = "public class A { class I {} }";
+		String v2 = "public class A { class J {} }";
+
+		assertNoBC(buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_protected_in_class_public_removed() {
+		String v1 = "public class A { protected class I {} }";
+		String v2 = "public class A { protected class J {} }";
+
+		assertBC("A$I", CLASS_REMOVED, 1, buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_public_in_class_public_removed() {
+		String v1 = "public class A { public class I {} }";
+		String v2 = "public class A { public class J {} }";
+
+		assertBC("A$I", CLASS_REMOVED, 1, buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_static_public_in_class_private_removed() {
+		String v1 = "class A { public static class I {} }";
+		String v2 = "class A { public static class J {} }";
+
+		assertNoBC(buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_static_protected_in_class_public_removed() {
+		String v1 = "public class A { static protected class I {} }";
+		String v2 = "public class A { static protected class J {} }";
+
+		assertBC("A$I", CLASS_REMOVED, 1, buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_static_public_in_class_public_removed() {
+		String v1 = "public class A { public static class I {} }";
+		String v2 = "public class A { public static class J {} }";
+
+		assertBC("A$I", CLASS_REMOVED, 1, buildDiff(v1, v2));
+	}
+
+	@Test
+	void class_inner_public_static_in_class_private_removed() {
+		String v1 = "class A { public static class I {} }";
+		String v2 = "class A { public static class J {} }";
+
+		assertNoBC(buildDiff(v1, v2));
 	}
 }
--- src/test/java/com/github/maracas/roseau/diff/DiffTestUtils.java
+++ src/test/java/com/github/maracas/roseau/diff/DiffTestUtils.java
@@ -1,4 +1,59 @@
 package com.github.maracas.roseau.diff;
 
-public class DiffTestUtils {
+import com.github.maracas.roseau.api.APIExtractor;
+import com.github.maracas.roseau.api.SpoonAPIExtractor;
+import com.github.maracas.roseau.diff.changes.BreakingChange;
+import com.github.maracas.roseau.diff.changes.BreakingChangeKind;
+import org.opentest4j.AssertionFailedError;
+import spoon.Launcher;
+import spoon.reflect.CtModel;
+import spoon.support.compiler.VirtualFile;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+class DiffTestUtils {
+	static void assertBC(String symbol, BreakingChangeKind kind, int line, List<BreakingChange> bcs) {
+		Optional<BreakingChange> matches = bcs.stream()
+			.filter(bc ->
+				   kind == bc.kind()
+				&& line == bc.location().beginLine()
+				&& symbol.equals(bc.impactedSymbol().getQualifiedName())
+			)
+			.findFirst();
+
+		if (matches.isEmpty()) {
+			String desc = "[%s, %s, %d]".formatted(symbol, kind, line);
+			String found = bcs.stream()
+				.filter(bc -> bc.location().beginLine() == line)
+				.map(bc -> "[%s, %s, %d]".formatted(bc.impactedSymbol().getQualifiedName(), bc.kind(), bc.location().beginLine()))
+				.collect(Collectors.joining(", "));
+			throw new AssertionFailedError("No breaking change", desc, found);
+		}
+	}
+
+	static void assertNoBC(List<BreakingChange> bcs) {
+		if (!bcs.isEmpty()) {
+			String found = bcs.stream()
+				.map(bc -> "[%s, %s, %d]".formatted(bc.impactedSymbol().getQualifiedName(), bc.kind(), bc.location().beginLine()))
+				.collect(Collectors.joining(", "));
+			throw new AssertionFailedError("Unexpected breaking change", "No breaking change", found);
+		}
+	}
+
+	static List<BreakingChange> buildDiff(String sourcesV1, String sourcesV2) {
+		APIExtractor extractor1 = new SpoonAPIExtractor(buildModel(sourcesV1));
+		APIExtractor extractor2 = new SpoonAPIExtractor(buildModel(sourcesV2));
+		return new APIDiff(extractor1.extractAPI(), extractor2.extractAPI()).diff();
+	}
+
+	static CtModel buildModel(String sources) {
+		Launcher launcher = new Launcher();
+
+		launcher.addInputResource(new VirtualFile(sources));
+		launcher.getEnvironment().setComplianceLevel(17);
+
+		return launcher.buildModel();
+	}
 }
