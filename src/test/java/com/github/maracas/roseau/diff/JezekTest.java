9530703e95e5f9ab186df11d64e9b26233fef737
Thomas Degueule <thomas.degueule@labri.fr>
2024-01-10 10:31:49 +0100

    JezekTest

---
+++ src/test/java/com/github/maracas/roseau/diff/JezekTest.java
@@ -0,0 +1,38 @@
+package com.github.maracas.roseau.diff;
+
+import com.github.maracas.roseau.diff.changes.BreakingChangeKind;
+import org.junit.jupiter.api.Test;
+
+import static com.github.maracas.roseau.TestUtils.assertBC;
+import static com.github.maracas.roseau.TestUtils.assertNoBC;
+import static com.github.maracas.roseau.TestUtils.buildDiff;
+
+class JezekTest {
+	@Test
+	void genericsWildcardsClazzConstructorParamLowerBoundsAdd() {
+		String v1 = """
+			public class A {
+			  public A(List<?> l) {}
+			}""";
+		String v2 = """
+			public class A {
+			  public A(List<A> l) {}
+			}""";
+
+		assertBC("A.<init>", BreakingChangeKind.CONSTRUCTOR_REMOVED, 2, buildDiff(v1, v2));
+	}
+
+	@Test
+	void circular() {
+		String v1 = """
+			public class A {
+			  public <T extends Number & Comparable<T>> void m() {}
+			}""";
+		String v2 = """
+			public class A {
+			  public <T extends Number & List<T>> void m() {}
+			}""";
+
+		assertBC("A.m", BreakingChangeKind.METHOD_FORMAL_TYPE_PARAMETERS_CHANGED, 2, buildDiff(v1, v2));
+	}
+}
