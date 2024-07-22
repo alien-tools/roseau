58570d75e3c00fb5587192aaddc0d732c325d63a
Thomas Degueule <thomas.degueule@labri.fr>
2024-07-22 16:39:40 +0200

    WIP

---
+++ generated/com/example/testapi/C67Sup.java
@@ -0,0 +1,799 @@
+package com.example.testapi;
+
+import java.lang.Integer;
+import java.lang.String;
+import java.util.List;
+
+sealed class C67Sup permits C67 {
+  private static int f4;
+
+  private static String f5;
+
+  private static List<String> f6;
+
+  private static Integer f7;
+
+  private static final int f12 = 0;
+
+  private static final String f13 = null;
+
+  private static final List<String> f14 = null;
+
+  private static final Integer f15 = null;
+
+  protected static int f20;
+
+  protected static String f21;
+
+  protected static List<String> f22;
+
+  protected static Integer f23;
+
+  protected static final int f28 = 0;
+
+  protected static final String f29 = null;
+
+  protected static final List<String> f30 = null;
+
+  protected static final Integer f31 = null;
+
+  public static int f36;
+
+  public static String f37;
+
+  public static List<String> f38;
+
+  public static Integer f39;
+
+  public static final int f44 = 0;
+
+  public static final String f45 = null;
+
+  public static final List<String> f46 = null;
+
+  public static final Integer f47 = null;
+
+  private int f0;
+
+  private String f1;
+
+  private List<String> f2;
+
+  private Integer f3;
+
+  private final int f8 = 0;
+
+  private final String f9 = null;
+
+  private final List<String> f10 = null;
+
+  private final Integer f11 = null;
+
+  protected int f16;
+
+  protected String f17;
+
+  protected List<String> f18;
+
+  protected Integer f19;
+
+  protected final int f24 = 0;
+
+  protected final String f25 = null;
+
+  protected final List<String> f26 = null;
+
+  protected final Integer f27 = null;
+
+  public int f32;
+
+  public String f33;
+
+  public List<String> f34;
+
+  public Integer f35;
+
+  public final int f40 = 0;
+
+  public final String f41 = null;
+
+  public final List<String> f42 = null;
+
+  public final Integer f43 = null;
+
+  private int m0() {
+    return 0;
+  }
+
+  private String m1() {
+    return null;
+  }
+
+  private List<String> m2() {
+    return null;
+  }
+
+  private Integer m3() {
+    return null;
+  }
+
+  private void m4() {
+  }
+
+  private final int m10() {
+    return 0;
+  }
+
+  private final String m11() {
+    return null;
+  }
+
+  private final List<String> m12() {
+    return null;
+  }
+
+  private final Integer m13() {
+    return null;
+  }
+
+  private final void m14() {
+  }
+
+  private synchronized int m40() {
+    return 0;
+  }
+
+  private synchronized String m41() {
+    return null;
+  }
+
+  private synchronized List<String> m42() {
+    return null;
+  }
+
+  private synchronized Integer m43() {
+    return null;
+  }
+
+  private synchronized void m44() {
+  }
+
+  private final synchronized int m50() {
+    return 0;
+  }
+
+  private final synchronized String m51() {
+    return null;
+  }
+
+  private final synchronized List<String> m52() {
+    return null;
+  }
+
+  private final synchronized Integer m53() {
+    return null;
+  }
+
+  private final synchronized void m54() {
+  }
+
+  private native int m80();
+
+  private native String m81();
+
+  private native List<String> m82();
+
+  private native Integer m83();
+
+  private native void m84();
+
+  private final native int m90();
+
+  private final native String m91();
+
+  private final native List<String> m92();
+
+  private final native Integer m93();
+
+  private final native void m94();
+
+  private synchronized native int m120();
+
+  private synchronized native String m121();
+
+  private synchronized native List<String> m122();
+
+  private synchronized native Integer m123();
+
+  private synchronized native void m124();
+
+  private final synchronized native int m130();
+
+  private final synchronized native String m131();
+
+  private final synchronized native List<String> m132();
+
+  private final synchronized native Integer m133();
+
+  private final synchronized native void m134();
+
+  private static int m160() {
+    return 0;
+  }
+
+  private static String m161() {
+    return null;
+  }
+
+  private static List<String> m162() {
+    return null;
+  }
+
+  private static Integer m163() {
+    return null;
+  }
+
+  private static void m164() {
+  }
+
+  private static final int m170() {
+    return 0;
+  }
+
+  private static final String m171() {
+    return null;
+  }
+
+  private static final List<String> m172() {
+    return null;
+  }
+
+  private static final Integer m173() {
+    return null;
+  }
+
+  private static final void m174() {
+  }
+
+  private static synchronized int m200() {
+    return 0;
+  }
+
+  private static synchronized String m201() {
+    return null;
+  }
+
+  private static synchronized List<String> m202() {
+    return null;
+  }
+
+  private static synchronized Integer m203() {
+    return null;
+  }
+
+  private static synchronized void m204() {
+  }
+
+  private static final synchronized int m210() {
+    return 0;
+  }
+
+  private static final synchronized String m211() {
+    return null;
+  }
+
+  private static final synchronized List<String> m212() {
+    return null;
+  }
+
+  private static final synchronized Integer m213() {
+    return null;
+  }
+
+  private static final synchronized void m214() {
+  }
+
+  private static native int m240();
+
+  private static native String m241();
+
+  private static native List<String> m242();
+
+  private static native Integer m243();
+
+  private static native void m244();
+
+  private static final native int m250();
+
+  private static final native String m251();
+
+  private static final native List<String> m252();
+
+  private static final native Integer m253();
+
+  private static final native void m254();
+
+  private static synchronized native int m280();
+
+  private static synchronized native String m281();
+
+  private static synchronized native List<String> m282();
+
+  private static synchronized native Integer m283();
+
+  private static synchronized native void m284();
+
+  private static final synchronized native int m290();
+
+  private static final synchronized native String m291();
+
+  private static final synchronized native List<String> m292();
+
+  private static final synchronized native Integer m293();
+
+  private static final synchronized native void m294();
+
+  protected int m320() {
+    return 0;
+  }
+
+  protected String m321() {
+    return null;
+  }
+
+  protected List<String> m322() {
+    return null;
+  }
+
+  protected Integer m323() {
+    return null;
+  }
+
+  protected void m324() {
+  }
+
+  protected final int m330() {
+    return 0;
+  }
+
+  protected final String m331() {
+    return null;
+  }
+
+  protected final List<String> m332() {
+    return null;
+  }
+
+  protected final Integer m333() {
+    return null;
+  }
+
+  protected final void m334() {
+  }
+
+  protected synchronized int m360() {
+    return 0;
+  }
+
+  protected synchronized String m361() {
+    return null;
+  }
+
+  protected synchronized List<String> m362() {
+    return null;
+  }
+
+  protected synchronized Integer m363() {
+    return null;
+  }
+
+  protected synchronized void m364() {
+  }
+
+  protected final synchronized int m370() {
+    return 0;
+  }
+
+  protected final synchronized String m371() {
+    return null;
+  }
+
+  protected final synchronized List<String> m372() {
+    return null;
+  }
+
+  protected final synchronized Integer m373() {
+    return null;
+  }
+
+  protected final synchronized void m374() {
+  }
+
+  protected native int m400();
+
+  protected native String m401();
+
+  protected native List<String> m402();
+
+  protected native Integer m403();
+
+  protected native void m404();
+
+  protected final native int m410();
+
+  protected final native String m411();
+
+  protected final native List<String> m412();
+
+  protected final native Integer m413();
+
+  protected final native void m414();
+
+  protected synchronized native int m440();
+
+  protected synchronized native String m441();
+
+  protected synchronized native List<String> m442();
+
+  protected synchronized native Integer m443();
+
+  protected synchronized native void m444();
+
+  protected final synchronized native int m450();
+
+  protected final synchronized native String m451();
+
+  protected final synchronized native List<String> m452();
+
+  protected final synchronized native Integer m453();
+
+  protected final synchronized native void m454();
+
+  protected static int m480() {
+    return 0;
+  }
+
+  protected static String m481() {
+    return null;
+  }
+
+  protected static List<String> m482() {
+    return null;
+  }
+
+  protected static Integer m483() {
+    return null;
+  }
+
+  protected static void m484() {
+  }
+
+  protected static final int m490() {
+    return 0;
+  }
+
+  protected static final String m491() {
+    return null;
+  }
+
+  protected static final List<String> m492() {
+    return null;
+  }
+
+  protected static final Integer m493() {
+    return null;
+  }
+
+  protected static final void m494() {
+  }
+
+  protected static synchronized int m520() {
+    return 0;
+  }
+
+  protected static synchronized String m521() {
+    return null;
+  }
+
+  protected static synchronized List<String> m522() {
+    return null;
+  }
+
+  protected static synchronized Integer m523() {
+    return null;
+  }
+
+  protected static synchronized void m524() {
+  }
+
+  protected static final synchronized int m530() {
+    return 0;
+  }
+
+  protected static final synchronized String m531() {
+    return null;
+  }
+
+  protected static final synchronized List<String> m532() {
+    return null;
+  }
+
+  protected static final synchronized Integer m533() {
+    return null;
+  }
+
+  protected static final synchronized void m534() {
+  }
+
+  protected static native int m560();
+
+  protected static native String m561();
+
+  protected static native List<String> m562();
+
+  protected static native Integer m563();
+
+  protected static native void m564();
+
+  protected static final native int m570();
+
+  protected static final native String m571();
+
+  protected static final native List<String> m572();
+
+  protected static final native Integer m573();
+
+  protected static final native void m574();
+
+  protected static synchronized native int m600();
+
+  protected static synchronized native String m601();
+
+  protected static synchronized native List<String> m602();
+
+  protected static synchronized native Integer m603();
+
+  protected static synchronized native void m604();
+
+  protected static final synchronized native int m610();
+
+  protected static final synchronized native String m611();
+
+  protected static final synchronized native List<String> m612();
+
+  protected static final synchronized native Integer m613();
+
+  protected static final synchronized native void m614();
+
+  public int m640() {
+    return 0;
+  }
+
+  public String m641() {
+    return null;
+  }
+
+  public List<String> m642() {
+    return null;
+  }
+
+  public Integer m643() {
+    return null;
+  }
+
+  public void m644() {
+  }
+
+  public final int m650() {
+    return 0;
+  }
+
+  public final String m651() {
+    return null;
+  }
+
+  public final List<String> m652() {
+    return null;
+  }
+
+  public final Integer m653() {
+    return null;
+  }
+
+  public final void m654() {
+  }
+
+  public synchronized int m680() {
+    return 0;
+  }
+
+  public synchronized String m681() {
+    return null;
+  }
+
+  public synchronized List<String> m682() {
+    return null;
+  }
+
+  public synchronized Integer m683() {
+    return null;
+  }
+
+  public synchronized void m684() {
+  }
+
+  public final synchronized int m690() {
+    return 0;
+  }
+
+  public final synchronized String m691() {
+    return null;
+  }
+
+  public final synchronized List<String> m692() {
+    return null;
+  }
+
+  public final synchronized Integer m693() {
+    return null;
+  }
+
+  public final synchronized void m694() {
+  }
+
+  public native int m720();
+
+  public native String m721();
+
+  public native List<String> m722();
+
+  public native Integer m723();
+
+  public native void m724();
+
+  public final native int m730();
+
+  public final native String m731();
+
+  public final native List<String> m732();
+
+  public final native Integer m733();
+
+  public final native void m734();
+
+  public synchronized native int m760();
+
+  public synchronized native String m761();
+
+  public synchronized native List<String> m762();
+
+  public synchronized native Integer m763();
+
+  public synchronized native void m764();
+
+  public final synchronized native int m770();
+
+  public final synchronized native String m771();
+
+  public final synchronized native List<String> m772();
+
+  public final synchronized native Integer m773();
+
+  public final synchronized native void m774();
+
+  public static int m800() {
+    return 0;
+  }
+
+  public static String m801() {
+    return null;
+  }
+
+  public static List<String> m802() {
+    return null;
+  }
+
+  public static Integer m803() {
+    return null;
+  }
+
+  public static void m804() {
+  }
+
+  public static final int m810() {
+    return 0;
+  }
+
+  public static final String m811() {
+    return null;
+  }
+
+  public static final List<String> m812() {
+    return null;
+  }
+
+  public static final Integer m813() {
+    return null;
+  }
+
+  public static final void m814() {
+  }
+
+  public static synchronized int m840() {
+    return 0;
+  }
+
+  public static synchronized String m841() {
+    return null;
+  }
+
+  public static synchronized List<String> m842() {
+    return null;
+  }
+
+  public static synchronized Integer m843() {
+    return null;
+  }
+
+  public static synchronized void m844() {
+  }
+
+  public static final synchronized int m850() {
+    return 0;
+  }
+
+  public static final synchronized String m851() {
+    return null;
+  }
+
+  public static final synchronized List<String> m852() {
+    return null;
+  }
+
+  public static final synchronized Integer m853() {
+    return null;
+  }
+
+  public static final synchronized void m854() {
+  }
+
+  public static native int m880();
+
+  public static native String m881();
+
+  public static native List<String> m882();
+
+  public static native Integer m883();
+
+  public static native void m884();
+
+  public static final native int m890();
+
+  public static final native String m891();
+
+  public static final native List<String> m892();
+
+  public static final native Integer m893();
+
+  public static final native void m894();
+
+  public static synchronized native int m920();
+
+  public static synchronized native String m921();
+
+  public static synchronized native List<String> m922();
+
+  public static synchronized native Integer m923();
+
+  public static synchronized native void m924();
+
+  public static final synchronized native int m930();
+
+  public static final synchronized native String m931();
+
+  public static final synchronized native List<String> m932();
+
+  public static final synchronized native Integer m933();
+
+  public static final synchronized native void m934();
+}
---
+++ src/test/java/com/github/maracas/roseau/Combinatorial.java
@@ -0,0 +1,218 @@
+package com.github.maracas.roseau;
+
+import com.google.common.collect.Sets;
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.FieldSpec;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.ParameterizedTypeName;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Combinatorial {
+	private static final String packageName = "com.example.testapi";
+	private static final Set<Modifier> visibilities = Set.of(Modifier.PRIVATE, Modifier.PROTECTED, Modifier.PUBLIC);
+	private static final Set<Modifier> fieldModifiers = Set.of(Modifier.STATIC, Modifier.FINAL); // TRANSIENT, VOLATILE
+	private static final Set<Modifier> methodModifiers = Set.of(Modifier.STATIC, Modifier.FINAL, Modifier.ABSTRACT, Modifier.NATIVE, Modifier.DEFAULT, Modifier.SYNCHRONIZED);
+	private static final Set<Modifier> typeModifiers = Set.of(Modifier.STATIC, Modifier.FINAL, Modifier.ABSTRACT, Modifier.SEALED, Modifier.NON_SEALED);
+	private static final Set<TypeName> fieldTypes = Set.of(
+		TypeName.INT,
+		TypeName.INT.box(),
+		ClassName.get(String.class),
+		ParameterizedTypeName.get(List.class, String.class)
+	);
+	private static final Set<TypeName> methodTypes = Sets.union(fieldTypes, Set.of(TypeName.VOID));
+
+	private Map<String, TypeSpec.Builder> types = new HashMap<>();
+
+	public static void main(String[] args) throws IOException {
+		new Combinatorial().run(packageName, Path.of("generated"));
+	}
+
+	public void run(String packageName, Path outputDir) throws IOException {
+		generateTypes();
+
+		for (var type : types.values().stream()
+			.sorted((t1, t2) -> {
+				if (t1.build().superclass.toString().equals(packageName + "." + t2.build().name)) {
+					return 1;
+				}
+				if (t2.build().superclass.toString().equals(packageName + "." + t1.build().name)) {
+					return -1;
+				}
+				return t1.build().name.toString().compareTo(t2.build().name.toString());
+			}).toList()) {
+			insertAllFields(type);
+			insertAllMethods(type);
+
+			var javaFile = JavaFile.builder(packageName, type.build()).build();
+			javaFile.writeTo(outputDir);
+		}
+	}
+
+	private void generateTypes() {
+		classes(null);
+	}
+
+	private void classes(TypeSpec.Builder parent) {
+		int i = 0;
+		for (var vis : visibilities) {
+			for (var mods : Sets.powerSet(typeModifiers)) {
+				var clsName = "C" + i++;
+
+				if (parent == null && (vis == Modifier.PRIVATE || vis == Modifier.PROTECTED || mods.contains(Modifier.STATIC)))
+					continue;
+				if (mods.contains(Modifier.SEALED) && mods.contains(Modifier.NON_SEALED))
+					continue;
+				if (mods.contains(Modifier.ABSTRACT) && mods.contains(Modifier.FINAL))
+					continue;
+				if (mods.contains(Modifier.FINAL) && (mods.contains(Modifier.SEALED) || mods.contains(Modifier.NON_SEALED)))
+					continue;
+
+				var cls = TypeSpec.classBuilder(clsName)
+					.addModifiers(vis)
+					.addModifiers(mods.toArray(new Modifier[0]));
+
+				if (mods.contains(Modifier.SEALED)) {
+					insertSubclassesSealed(cls);
+				}
+
+				if (mods.contains(Modifier.NON_SEALED)) {
+					insertSuperclassSealed(cls);
+				}
+
+				storeType(clsName, cls);
+			}
+		}
+	}
+
+	private void insertAllFields(TypeSpec.Builder typeBuilder) {
+		int i = 0;
+		for (var vis : visibilities) {
+			for (var mods : Sets.powerSet(fieldModifiers)) {
+				for (var t : fieldTypes) {
+					var fName = "f" + i++;
+					var f = FieldSpec.builder(t, fName)
+						.addModifiers(vis)
+						.addModifiers(mods.toArray(new Modifier[0]));
+
+					if (mods.contains(Modifier.FINAL))
+						f.initializer("$L", getDefaultValue(t));
+
+					typeBuilder.addField(f.build());
+				}
+			}
+		}
+	}
+
+	private void insertAllMethods(TypeSpec.Builder typeBuilder) {
+		int i = 0;
+		for (var vis : visibilities) {
+			for (var mods : Sets.powerSet(methodModifiers)) {
+				for (var t : methodTypes) {
+					var mName = "m" + i++;
+
+					if (mods.contains(Modifier.DEFAULT) && typeBuilder.build().kind != TypeSpec.Kind.INTERFACE)
+						continue;
+					if (mods.contains(Modifier.ABSTRACT) && (vis == Modifier.PRIVATE ||
+						mods.contains(Modifier.FINAL) || mods.contains(Modifier.NATIVE) || mods.contains(Modifier.STATIC) || mods.contains(Modifier.SYNCHRONIZED)))
+						continue;
+
+					var m = MethodSpec.methodBuilder(mName)
+						.addModifiers(vis)
+						.addModifiers(mods)
+						.returns(t);
+
+					if (!mods.contains(Modifier.ABSTRACT) && !mods.contains(Modifier.NATIVE) && !t.equals(TypeName.VOID))
+						m.addCode("return $L;", getDefaultValue(t));
+
+					var shouldOverride = false;
+					var sup = types.get(typeBuilder.build().superclass.toString());
+					if (sup != null) {
+						var opt = sup.methodSpecs.stream().filter(superM -> superM.name.equals(mName)).findFirst();
+						if (opt.isPresent()) {
+							var superM = opt.get();
+							if (superM.hasModifier(Modifier.FINAL))
+								continue;
+							if (superM.hasModifier(Modifier.PUBLIC) && mods.contains(Modifier.PROTECTED))
+								continue;
+							if (superM.hasModifier(Modifier.ABSTRACT) && !typeBuilder.modifiers.contains(Modifier.ABSTRACT)) {
+								shouldOverride = true;
+							}
+						}
+					}
+
+					if (shouldOverride) {
+						m.modifiers.remove(Modifier.ABSTRACT);
+						if (!t.equals(TypeName.VOID))
+							m.addCode("return $L;", getDefaultValue(t));
+					} else if (mods.contains(Modifier.ABSTRACT) && !typeBuilder.modifiers.contains(Modifier.ABSTRACT)) {
+						continue;
+					}
+
+					typeBuilder.addMethod(m.build());
+					typeBuilder.build();
+				}
+			}
+		}
+	}
+
+	private void insertSubclassesSealed(TypeSpec.Builder cls) {
+		// FIXME
+		String supName = cls.build().name;
+		String subName = "Sub" + supName;
+		var sub = TypeSpec.classBuilder(subName)
+			.addModifiers(Modifier.FINAL)
+			.superclass(ClassName.get(packageName, supName));
+
+		cls.addPermits(ClassName.get(packageName, subName));
+
+		storeType(subName, sub);
+	}
+
+	private void insertSuperclassSealed(TypeSpec.Builder cls) {
+		String subName = cls.build().name;
+		String supName = subName + "Sup";
+
+		// FIXME
+		var sup = TypeSpec.classBuilder(supName)
+			.addModifiers(Modifier.SEALED)
+			.addPermits(ClassName.get(packageName, subName));
+
+		cls.superclass(ClassName.get(packageName, supName));
+
+		storeType(supName, sup);
+	}
+
+	private String getDefaultValue(TypeName type) {
+		if (type.equals(TypeName.BOOLEAN)) return "false";
+		if (type.equals(TypeName.BYTE) || type.equals(TypeName.SHORT) || type.equals(TypeName.INT) || type.equals(TypeName.LONG)) return "0";
+		if (type.equals(TypeName.FLOAT)) return "0.0f";
+		if (type.equals(TypeName.DOUBLE)) return "0.0d";
+		if (type.equals(TypeName.CHAR)) return "'\\u0000'";
+		return "null";
+	}
+
+	private boolean isAbstract(TypeSpec.Builder typeBuilder) {
+		return typeBuilder.modifiers.contains(Modifier.ABSTRACT);
+	}
+
+	private TypeSpec.Builder findType(String name) {
+		if (name == null)
+			return null;
+
+		return types.get(name);
+	}
+
+	private void storeType(String name, TypeSpec.Builder typeBuilder) {
+		types.put(packageName + "." + name, typeBuilder);
+	}
+}
