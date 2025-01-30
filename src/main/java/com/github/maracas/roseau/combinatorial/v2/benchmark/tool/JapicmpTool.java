e4c4dbdb808b41b2e70baa61d7673217350c78ce
Corentin Latappy <corentin.latappy@labri.fr>
2025-01-31 00:35:15 +0100

    🚀 Implementing japicmp tool benchmark

--- src/main/java/com/github/maracas/roseau/combinatorial/v2/benchmark/tool/JapicmpTool.java
+++ src/main/java/com/github/maracas/roseau/combinatorial/v2/benchmark/tool/JapicmpTool.java
@@ -1,8 +1,13 @@
 package com.github.maracas.roseau.combinatorial.v2.benchmark.tool;
 
 import com.github.maracas.roseau.combinatorial.v2.benchmark.ToolResult;
+import japicmp.cmp.JApiCmpArchive;
+import japicmp.cmp.JarArchiveComparator;
+import japicmp.cmp.JarArchiveComparatorOptions;
+import japicmp.model.JApiClass;
 
 import java.nio.file.Path;
+import java.util.List;
 
 public final class JapicmpTool extends AbstractTool {
 	public JapicmpTool(Path v1Path, Path v2Path) {
@@ -11,10 +16,16 @@ public final class JapicmpTool extends AbstractTool {
 
 	@Override
 	public ToolResult detectBreakingChanges() {
+		var comparatorOptions = new JarArchiveComparatorOptions();
+		var jarArchiveComparator = new JarArchiveComparator(comparatorOptions);
+
+		var v1Archive = new JApiCmpArchive(v1Path.toFile(), "1.0.0");
+		var v2Archive = new JApiCmpArchive(v2Path.toFile(), "2.0.0");
+
 		long startTime = System.currentTimeMillis();
-		// TODO: Implement Japicmp
+		List<JApiClass> jApiClasses = jarArchiveComparator.compare(v1Archive, v2Archive);
 		long executionTime = System.currentTimeMillis() - startTime;
 
-		return new ToolResult(executionTime, false);
+		return new ToolResult(executionTime, !jApiClasses.isEmpty());
 	}
 }
