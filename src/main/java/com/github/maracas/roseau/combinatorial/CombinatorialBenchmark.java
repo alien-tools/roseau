f60402f7e31320adfc4726b7d56ff8a8ed6873f9
Corentin Latappy <corentin.latappy@labri.fr>
2025-01-31 17:32:24 +0100

    🚀 Exporting generated API to JSON

--- src/main/java/com/github/maracas/roseau/combinatorial/CombinatorialBenchmark.java
+++ src/main/java/com/github/maracas/roseau/combinatorial/CombinatorialBenchmark.java
@@ -1,10 +1,11 @@
 package com.github.maracas.roseau.combinatorial;
 
 import com.github.maracas.roseau.api.SpoonAPIExtractor;
-import com.github.maracas.roseau.combinatorial.api.GenerateCombinatorialApi;
+import com.github.maracas.roseau.api.model.API;
 import com.github.maracas.roseau.combinatorial.client.GenerateApiClients;
 import com.github.maracas.roseau.combinatorial.v2.GenerateNewVersionsAndLaunchBenchmark;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 public final class CombinatorialBenchmark {
@@ -19,12 +20,10 @@ public final class CombinatorialBenchmark {
 		var outputPath = Path.of(outputDir);
 
 		try {
-			var apiGeneration = new GenerateCombinatorialApi(outputPath);
-			apiGeneration.run();
+//			var apiGeneration = new GenerateCombinatorialApi(outputPath);
+//			apiGeneration.run();
 
-			var apiPath = outputPath.resolve(Constants.API_FOLDER);
-			var apiExtractor = new SpoonAPIExtractor();
-			var api = apiExtractor.extractAPI(apiPath);
+			var api = getAndExportGeneratedApi(outputPath);
 
 			var clientsGeneration = new GenerateApiClients(api, outputPath);
 			clientsGeneration.run();
@@ -37,5 +36,20 @@ public final class CombinatorialBenchmark {
 
 			System.exit(1);
 		}
+	}
+
+	private static API getAndExportGeneratedApi(Path outputPath) {
+		var apiPath = outputPath.resolve(Constants.API_FOLDER);
+		var apiExtractor = new SpoonAPIExtractor();
+		var api = apiExtractor.extractAPI(apiPath);
+
+		try {
+			var exportPath = outputPath.resolve("api.json");
+			api.writeJson(exportPath);
+		} catch (IOException e) {
+			throw new RuntimeException("Failed to export generated API", e);
+		}
+
+		return api;
 	}
 }
