82d9c928d3271d6f3c48a2348a4c8bac600dac0a
Corentin Latappy <corentin.latappy@labri.fr>
2025-02-10 15:35:51 +0100

    🐛 Fixing Japicmp BC detection

--- src/main/java/com/github/maracas/roseau/combinatorial/v2/benchmark/tool/JapicmpTool.java
+++ src/main/java/com/github/maracas/roseau/combinatorial/v2/benchmark/tool/JapicmpTool.java
@@ -4,6 +4,8 @@ import com.github.maracas.roseau.combinatorial.v2.benchmark.result.ToolResult;
 import japicmp.cmp.JApiCmpArchive;
 import japicmp.cmp.JarArchiveComparator;
 import japicmp.cmp.JarArchiveComparatorOptions;
+import japicmp.config.Options;
+import japicmp.model.AccessModifier;
 import japicmp.model.JApiClass;
 
 import java.nio.file.Path;
@@ -18,14 +20,22 @@ public final class JapicmpTool extends AbstractTool {
 	public ToolResult detectBreakingChanges() {
 		long startTime = System.currentTimeMillis();
 
-		var comparatorOptions = new JarArchiveComparatorOptions();
+		Options opts = Options.newDefault();
+		opts.setAccessModifier(AccessModifier.PACKAGE_PROTECTED);
+		opts.setOutputOnlyModifications(true);
+		opts.setIgnoreMissingClasses(true);
+		opts.setIncludeSynthetic(true);
+		var comparatorOptions = JarArchiveComparatorOptions.of(opts);
 		var jarArchiveComparator = new JarArchiveComparator(comparatorOptions);
 
 		var v1Archive = new JApiCmpArchive(v1Path.toFile(), "1.0.0");
 		var v2Archive = new JApiCmpArchive(v2Path.toFile(), "2.0.0");
 
 		List<JApiClass> jApiClasses = jarArchiveComparator.compare(v1Archive, v2Archive);
-		var isBreaking = !jApiClasses.isEmpty();
+		var realBreakingClasses = jApiClasses.stream()
+				.filter(cls -> !cls.isBinaryCompatible() || !cls.isSourceCompatible())
+				.toList();
+		var isBreaking = !realBreakingClasses.isEmpty();
 
 		long executionTime = System.currentTimeMillis() - startTime;
 		return new ToolResult("japicmp", executionTime, isBreaking);
