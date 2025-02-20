303815beb0e5590f345c34000be6d2dd8b2b9059
Thomas Degueule <thomas.degueule@labri.fr>
2025-02-20 21:08:40 +0100

    WIP

--- src/main/java/com/github/maracas/roseau/WalkRepository.java
+++ src/main/java/com/github/maracas/roseau/WalkRepository.java
@@ -3,8 +3,9 @@ package com.github.maracas.roseau;
 import com.github.maracas.roseau.api.model.API;
 import com.github.maracas.roseau.diff.APIDiff;
 import com.github.maracas.roseau.diff.changes.BreakingChange;
-import com.github.maracas.roseau.extractors.sources.SpoonAPIExtractor;
-import com.github.maracas.roseau.extractors.sources.SpoonUtils;
+import com.github.maracas.roseau.extractors.TimestampChangedFilesProvider;
+import com.github.maracas.roseau.extractors.jdt.IncrementalJdtAPIExtractor;
+import com.github.maracas.roseau.extractors.jdt.JdtAPIExtractor;
 import com.google.common.base.Stopwatch;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
@@ -17,7 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,11 +29,11 @@ public class WalkRepository {
 		// walk("https://github.com/alien-tools/roseau", Path.of("walk-roseau"), "main", "v0.0.2", "src/main/java");
 
 		// Guava
-		// walk("https://github.com/google/guava", Path.of("walk-guava"), "master", "v10.0", "guava/src/main/java");
+		walk("https://github.com/google/guava", Path.of("walk-guava"), "master", "v10.0", List.of("guava/src"));
 
 		// commons-lang
-		walk("https://github.com/apache/commons-lang", Path.of("walk-commons-lang"), "master",
-			"LANG_1_0_B1", List.of("src/main/java", "src/java"));
+		// walk("https://github.com/apache/commons-lang", Path.of("walk-commons-lang"), "master",
+		// 	"LANG_1_0_B1", List.of("src/main/java", "src/java"));
 	}
 
 	public static void walk(String url, Path clone, String branch, String tagName, List<String> srcRoot) throws Exception {
@@ -41,7 +42,7 @@ public class WalkRepository {
 
 		// Clone the repository if it doesn't exist
 		if (!repoDir.exists()) {
-			System.out.printf("Cloning %s%n", url);
+			System.out.printf("Cloning %s...%n", url);
 			git = Git.cloneRepository()
 				.setURI(url)
 				.setDirectory(repoDir)
@@ -70,10 +71,12 @@ public class WalkRepository {
 		walk.markStart(head);
 		walk.markUninteresting(tag);
 
-		var extractor = new SpoonAPIExtractor();
+		var extractor = new JdtAPIExtractor();
 		RevCommit commit = null;
 		API previousApi = null;
 		var sw = Stopwatch.createUnstarted();
+		var provider = new TimestampChangedFilesProvider(clone.resolve(srcRoot.getFirst()).toAbsolutePath());
+		var incrementalExtractor = new IncrementalJdtAPIExtractor();
 		while ((commit = walk.next()) != null) {
 			Date commitDate = Date.from(commit.getAuthorIdent().getWhenAsInstant());
 
@@ -83,20 +86,33 @@ public class WalkRepository {
 			var checkoutTime = sw.elapsed().toMillis();
 			System.out.printf(" done in %sms%n", checkoutTime);
 
-			System.out.print("Parsing...");
-			sw.reset().start();
-			var model =
-				Files.exists(clone.resolve(srcRoot.get(0)))
-					? SpoonUtils.buildModel(clone.resolve(srcRoot.get(0)), Duration.ofMinutes(1))
-					: SpoonUtils.buildModel(clone.resolve(srcRoot.get(1)), Duration.ofMinutes(1));
-			var parsingTime = sw.elapsed().toMillis();
-			System.out.printf(" done in %sms%n", parsingTime);
-
-			System.out.print("Extracting API...");
-			sw.reset().start();
-			var api = extractor.extractAPI(model);
-			var apiTime = sw.elapsed().toMillis();
-			System.out.printf(" done in %sms%n", apiTime);
+			API api = null;
+			long apiTime = 0;
+			var changedFiles = provider.getChangedFiles();
+			if (previousApi == null) {
+				System.out.print("Extracting API...");
+				sw.reset().start();
+				api = Files.exists(clone.resolve(srcRoot.get(0)))
+					? extractor.extractAPI(clone.resolve(srcRoot.get(0)))
+					: extractor.extractAPI(clone.resolve(srcRoot.get(1)));
+				apiTime = sw.elapsed().toMillis();
+				System.out.printf(" done in %sms%n", apiTime);
+			} else {
+				System.out.printf("Partial update:%n\t%d created: %s%n\t%d removed: %s%n\t%d changed: %s%n",
+					changedFiles.createdFiles().size(), changedFiles.createdFiles().stream()
+						.map(p -> p.getFileName().toString()).collect(Collectors.joining(", ")),
+					changedFiles.deletedFiles().size(), changedFiles.deletedFiles().stream()
+						.map(p -> p.getFileName().toString()).collect(Collectors.joining(", ")),
+					changedFiles.updatedFiles().size(), changedFiles.updatedFiles().stream()
+						.map(p -> p.getFileName().toString()).collect(Collectors.joining(", ")));
+				System.out.print("Extracting partial API...");
+				sw.reset().start();
+				api = Files.exists(clone.resolve(srcRoot.get(0)))
+					? incrementalExtractor.refreshAPI(clone.resolve(srcRoot.get(0)), changedFiles, previousApi)
+					: incrementalExtractor.refreshAPI(clone.resolve(srcRoot.get(1)), changedFiles, previousApi);
+				apiTime = sw.elapsed().toMillis();
+				System.out.printf(" done in %sms%n", apiTime);
+			}
 
 			if (previousApi != null) {
 				System.out.print("Diffing...");
@@ -114,14 +130,18 @@ public class WalkRepository {
 					.mapToInt(type -> type.getDeclaredFields().size())
 					.sum();
 
-				var line = "%s|%s|%s|%d|%d|%d|%d|%d|%d|%d|%d|%s%n".formatted(commit.getName(), commitDate,
-					commit.getShortMessage().replace("|", ""), numTypes, numMethods, numFields,
-					checkoutTime, parsingTime, apiTime, diffTime, bcs.size(),
+				var line = "%s|%s|%s|%d|%d|%d|%d|%d|%d|%d|%d|%d|%d|%s%n".formatted(commit.getName(), commitDate,
+					commit.getShortMessage().replace("|", ""),
+					changedFiles.createdFiles().size(), changedFiles.deletedFiles().size(), changedFiles.updatedFiles().size(),
+					numTypes, numMethods, numFields,
+					checkoutTime, apiTime, diffTime, bcs.size(),
 					bcs.stream().map(BreakingChange::toString).collect(Collectors.joining(",")));
 				Files.write(Path.of("git.csv"), line.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
 			}
 
 			previousApi = api;
+			provider.refresh(previousApi, Instant.now().toEpochMilli());
 		}
 	}
 }
+
