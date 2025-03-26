b91f34c167da83363101f9150e9fed2a5beafcdc
Corentin Latappy <corentin.latappy@labri.fr>
2025-03-26 11:33:38 +0100

    ♻️ Refactoring to avoid code duplication to AddMethod to InterfaceDecl

--- core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/cls/AddMethodAbstractClassStrategy.java
+++ core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/cls/AddMethodAbstractClassStrategy.java
@@ -1,35 +1,21 @@
 package io.github.alien.roseau.combinatorial.v2.breaker.cls;
 
-import io.github.alien.roseau.api.model.AccessModifier;
 import io.github.alien.roseau.api.model.ClassDecl;
 import io.github.alien.roseau.api.model.Modifier;
-import io.github.alien.roseau.api.model.reference.PrimitiveTypeReference;
 import io.github.alien.roseau.combinatorial.builder.ApiBuilder;
-import io.github.alien.roseau.combinatorial.builder.MethodBuilder;
 import io.github.alien.roseau.combinatorial.v2.breaker.ImpossibleChangeException;
+import io.github.alien.roseau.combinatorial.v2.breaker.tpDcl.AddMethodTypeStrategy;
 import io.github.alien.roseau.combinatorial.v2.queue.NewApiQueue;
 
-public class AddMethodAbstractClassStrategy extends AbstractClsStrategy {
+public class AddMethodAbstractClassStrategy extends AddMethodTypeStrategy {
 	public AddMethodAbstractClassStrategy(ClassDecl cls, NewApiQueue queue) {
-		super(cls, queue, "AddMethodToAbstractClass%s".formatted(cls.getSimpleName()));
+		super(cls, queue);
 	}
 
 	@Override
 	protected void applyBreakToMutableApi(ApiBuilder mutableApi) throws ImpossibleChangeException {
-		if (!cls.getModifiers().contains(Modifier.ABSTRACT)) throw new ImpossibleChangeException();
+		if (!tp.getModifiers().contains(Modifier.ABSTRACT)) throw new ImpossibleChangeException();
 
-		LOGGER.info("Adding new method to abstract class {}", cls.getSimpleName());
-
-		var mutableClass = mutableApi.allTypes.get(cls.getQualifiedName());
-		if (mutableClass == null) throw new ImpossibleChangeException();
-
-		var methodBuilder = new MethodBuilder();
-		methodBuilder.qualifiedName = "%s.%s".formatted(cls.getQualifiedName(), "newMethodAddedToAbstractClass");
-		methodBuilder.visibility = AccessModifier.PUBLIC;
-		methodBuilder.modifiers.add(Modifier.ABSTRACT);
-		methodBuilder.containingType = mutableApi.typeReferenceFactory.createTypeReference(cls.getQualifiedName());
-		methodBuilder.type = new PrimitiveTypeReference("void");
-
-		mutableClass.methods.add(methodBuilder.make());
+		super.applyBreakToMutableApi(mutableApi);
 	}
 }
---
+++ core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/tpDcl/AddMethodTypeStrategy.java
@@ -0,0 +1,33 @@
+package io.github.alien.roseau.combinatorial.v2.breaker.tpDcl;
+
+import io.github.alien.roseau.api.model.AccessModifier;
+import io.github.alien.roseau.api.model.TypeDecl;
+import io.github.alien.roseau.api.model.Modifier;
+import io.github.alien.roseau.api.model.reference.PrimitiveTypeReference;
+import io.github.alien.roseau.combinatorial.builder.ApiBuilder;
+import io.github.alien.roseau.combinatorial.builder.MethodBuilder;
+import io.github.alien.roseau.combinatorial.v2.breaker.ImpossibleChangeException;
+import io.github.alien.roseau.combinatorial.v2.queue.NewApiQueue;
+
+public class AddMethodTypeStrategy extends AbstractTpStrategy {
+	public AddMethodTypeStrategy(TypeDecl tp, NewApiQueue queue) {
+		super(tp, queue, "AddMethodToType%s".formatted(tp.getSimpleName()));
+	}
+
+	@Override
+	protected void applyBreakToMutableApi(ApiBuilder mutableApi) throws ImpossibleChangeException {
+		LOGGER.info("Adding new method to type {}", tp.getSimpleName());
+
+		var mutableType = mutableApi.allTypes.get(tp.getQualifiedName());
+		if (mutableType == null) throw new ImpossibleChangeException();
+
+		var methodBuilder = new MethodBuilder();
+		methodBuilder.qualifiedName = "%s.%s".formatted(tp.getQualifiedName(), "newMethodAddedToType");
+		methodBuilder.visibility = AccessModifier.PUBLIC;
+		methodBuilder.modifiers.add(Modifier.ABSTRACT);
+		methodBuilder.containingType = mutableApi.typeReferenceFactory.createTypeReference(tp.getQualifiedName());
+		methodBuilder.type = new PrimitiveTypeReference("void");
+
+		mutableType.methods.add(methodBuilder.make());
+	}
+}
