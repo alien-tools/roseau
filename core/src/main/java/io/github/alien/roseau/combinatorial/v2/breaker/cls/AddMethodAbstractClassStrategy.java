e7d0294c148cc597ae30b1c2125d74fadb6b5a7e
Corentin Latappy <corentin.latappy@labri.fr>
2025-05-06 15:28:26 +0200

    🚀 Updating AddMethodTypeStrategy due to hierarchy changes

--- core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/cls/AddMethodAbstractClassStrategy.java
+++ core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/cls/AddMethodAbstractClassStrategy.java
@@ -1,10 +1,7 @@
 package io.github.alien.roseau.combinatorial.v2.breaker.cls;
 
 import io.github.alien.roseau.api.model.ClassDecl;
-import io.github.alien.roseau.api.model.Modifier;
 import io.github.alien.roseau.combinatorial.builder.ApiBuilder;
-import io.github.alien.roseau.combinatorial.builder.ClassBuilder;
-import io.github.alien.roseau.combinatorial.builder.MethodBuilder;
 import io.github.alien.roseau.combinatorial.v2.breaker.ImpossibleChangeException;
 import io.github.alien.roseau.combinatorial.v2.breaker.tp.AddMethodTypeStrategy;
 import io.github.alien.roseau.combinatorial.v2.queue.NewApiQueue;
@@ -19,21 +16,5 @@ public final class AddMethodAbstractClassStrategy extends AddMethodTypeStrategy<
 		if (!tp.isAbstract()) throw new ImpossibleChangeException();
 
 		super.applyBreakToMutableApi(mutableApi);
-
-		var mutableClass = getMutableClass(mutableApi);
-		var addedMethodBuilder = mutableClass.methods.getLast();
-
-		getAllOtherMutableTypes(mutableApi).forEach(typeBuilder -> {
-			if (typeBuilder instanceof ClassBuilder classBuilder) {
-				if (classBuilder.superClass != null && classBuilder.superClass.getQualifiedName().equals(tp.getQualifiedName())) {
-					if (!classBuilder.modifiers.contains(Modifier.ABSTRACT)) {
-						var newMethodBuilder = MethodBuilder.from(addedMethodBuilder.make());
-						newMethodBuilder.containingType = mutableApi.typeReferenceFactory.createTypeReference(classBuilder.qualifiedName);
-						newMethodBuilder.modifiers.remove(Modifier.ABSTRACT);
-						classBuilder.methods.add(newMethodBuilder);
-					}
-				}
-			}
-		});
 	}
 }
--- core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/tp/AddMethodTypeStrategy.java
+++ core/src/main/java/io/github/alien/roseau/combinatorial/v2/breaker/tp/AddMethodTypeStrategy.java
@@ -5,6 +5,7 @@ import io.github.alien.roseau.api.model.TypeDecl;
 import io.github.alien.roseau.api.model.Modifier;
 import io.github.alien.roseau.api.model.reference.PrimitiveTypeReference;
 import io.github.alien.roseau.combinatorial.builder.ApiBuilder;
+import io.github.alien.roseau.combinatorial.builder.ClassBuilder;
 import io.github.alien.roseau.combinatorial.builder.MethodBuilder;
 import io.github.alien.roseau.combinatorial.v2.breaker.ImpossibleChangeException;
 import io.github.alien.roseau.combinatorial.v2.queue.NewApiQueue;
@@ -18,8 +19,6 @@ public class AddMethodTypeStrategy<T extends TypeDecl> extends AbstractTpStrateg
 	protected void applyBreakToMutableApi(ApiBuilder mutableApi) throws ImpossibleChangeException {
 		LOGGER.info("Adding new method to type {}", tp.getSimpleName());
 
-		var mutableType = getMutableType(mutableApi);
-
 		var methodBuilder = new MethodBuilder();
 		methodBuilder.qualifiedName = "%s.%s".formatted(tp.getQualifiedName(), "newMethodAddedToType");
 		methodBuilder.visibility = AccessModifier.PUBLIC;
@@ -27,6 +26,23 @@ public class AddMethodTypeStrategy<T extends TypeDecl> extends AbstractTpStrateg
 		methodBuilder.containingType = mutableApi.typeReferenceFactory.createTypeReference(tp.getQualifiedName());
 		methodBuilder.type = new PrimitiveTypeReference("void");
 
+		var mutableType = getMutableType(mutableApi);
 		mutableType.methods.add(methodBuilder);
+
+		getAllOtherMutableTypes(mutableApi).forEach(typeBuilder -> {
+			if (typeBuilder instanceof ClassBuilder classBuilder) {
+				var hasCurrentTypeAsSuperClass = classBuilder.superClass != null && classBuilder.superClass.getQualifiedName().equals(tp.getQualifiedName());
+				var hasCurrentTypeAsImplementedInterface = classBuilder.implementedInterfaces.stream().anyMatch(i -> i.getQualifiedName().equals(tp.getQualifiedName()));
+
+				if (hasCurrentTypeAsSuperClass || hasCurrentTypeAsImplementedInterface) {
+					if (!classBuilder.modifiers.contains(Modifier.ABSTRACT)) {
+						var newMethodBuilder = MethodBuilder.from(methodBuilder.make());
+						newMethodBuilder.containingType = mutableApi.typeReferenceFactory.createTypeReference(classBuilder.qualifiedName);
+						newMethodBuilder.modifiers.remove(Modifier.ABSTRACT);
+						classBuilder.methods.add(newMethodBuilder);
+					}
+				}
+			}
+		});
 	}
 }
