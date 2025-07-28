81448e1d0eaba6f99cae9f7268cb1daeaaeb37c6
Corentin Latappy <corentin.latappy@labri.fr>
2025-07-28 15:17:29 +0200

    🐛 Fixing bounds formatting when generics are present

--- combinatorial/src/main/java/io/github/alien/roseau/combinatorial/GenerateClients.java
+++ combinatorial/src/main/java/io/github/alien/roseau/combinatorial/GenerateClients.java
@@ -26,7 +26,6 @@ public final class GenerateClients {
 			Arrays.stream(v1sPath.toFile().listFiles()).toList().forEach(v1Folder -> {
 				if (!v1Folder.isDirectory()) return;
 				var v1Path = v1Folder.toPath();
-				if (!v1Path.toString().endsWith("genericsClazzMethodTypeAddSecond")) return;
 
 				var types = new JdtTypesExtractor().extractTypes(v1Path);
 				if (types == null) {
