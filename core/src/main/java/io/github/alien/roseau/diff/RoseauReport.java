703e50b20da6f62c32a4dcfedad609e766d481db
Thomas Degueule <thomas.degueule@labri.fr>
2025-09-27 00:01:48 +0200

    Make the RoseauReport more deterministic by listing BCs following symbol names

--- core/src/main/java/io/github/alien/roseau/diff/RoseauReport.java
+++ core/src/main/java/io/github/alien/roseau/diff/RoseauReport.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import io.github.alien.roseau.api.model.API;
 import io.github.alien.roseau.diff.changes.BreakingChange;
 
+import java.util.Comparator;
 import java.util.List;
 
 public record RoseauReport(
@@ -15,6 +16,9 @@ public record RoseauReport(
 		Preconditions.checkNotNull(v1);
 		Preconditions.checkNotNull(v2);
 		Preconditions.checkNotNull(breakingChanges);
-		breakingChanges = java.util.List.copyOf(breakingChanges);
+		breakingChanges = java.util.List.copyOf(
+			breakingChanges.stream()
+				.sorted(Comparator.comparing(bc -> bc.impactedSymbol().getQualifiedName()))
+				.toList());
 	}
 }
