6761faddb3948cbb96a2c425e8d387d17ff20aa1
Thomas Degueule <thomas.degueule@labri.fr>
2025-10-20 23:26:21 +0200

    smoke tests: update libraries list, check unresolved references, check but don't fail on BCs, remove Spoon checks

--- core/src/test/java/io/github/alien/roseau/smoke/PopularLibrariesTestIT.java
+++ core/src/test/java/io/github/alien/roseau/smoke/PopularLibrariesTestIT.java
@@ -3,6 +3,11 @@ package io.github.alien.roseau.smoke;
 import com.google.common.base.Stopwatch;
 import io.github.alien.roseau.Library;
 import io.github.alien.roseau.Roseau;
+import io.github.alien.roseau.api.model.API;
+import io.github.alien.roseau.api.model.TypeDecl;
+import io.github.alien.roseau.api.model.reference.TypeReference;
+import io.github.alien.roseau.api.visit.AbstractAPIVisitor;
+import io.github.alien.roseau.api.visit.Visit;
 import io.github.alien.roseau.diff.changes.BreakingChange;
 import io.github.alien.roseau.extractors.ExtractorType;
 import io.github.alien.roseau.extractors.MavenClasspathBuilder;
@@ -22,6 +27,8 @@ import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.jar.JarEntry;
@@ -29,26 +36,26 @@ import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assumptions.assumeThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PopularLibrariesTestIT {
 	static Stream<String> libraries() {
 		return Stream.of(
+			"io.github.alien-tools:roseau-core:0.3.0", // ;)
 			"org.assertj:assertj-core:3.27.3",
 			"commons-codec:commons-codec:1.18.0",
 			"com.google.guava:guava:32.1.3-jre",
 			"org.apache.commons:commons-lang3:3.17.0",
 			"commons-io:commons-io:2.18.0",
 			"org.eclipse.collections:eclipse-collections-api:11.1.0",
-			"org.springframework:spring-core:6.1.5",
 			"io.dropwizard:dropwizard-core:4.0.1",
-			"io.projectreactor:reactor-core:3.6.3",
 			"org.reactivestreams:reactive-streams:1.0.4",
-			"org.apache.kafka:kafka-clients:3.6.0",
 			"com.google.code.gson:gson:2.10.1",
-			"org.junit.jupiter:junit-jupiter-api:5.10.1",
+			"org.junit.jupiter:junit-jupiter-api:5.14.0",
+			"org.junit.jupiter:junit-jupiter-engine:5.14.0",
 			"com.squareup:javapoet:1.13.0",
 			"org.jooq:joor-java-8:0.9.15",
 			"joda-time:joda-time:2.12.5",
@@ -56,30 +63,49 @@ class PopularLibrariesTestIT {
 			"com.google.dagger:dagger:2.55",
 			"ch.qos.logback:logback-core:1.5.16",
 			"ch.qos.logback:logback-classic:1.5.16",
-			"org.apache.logging.log4j:log4j-core:2.24.3", // external libs
+			"org.apache.logging.log4j:log4j-core:2.24.3",
 			"org.apache.logging.log4j:log4j-api:2.24.3",
 			"org.slf4j:slf4j-simple:2.0.16",
 			"org.slf4j:slf4j-api:2.0.16",
-			"com.fasterxml.jackson.core:jackson-core:2.18.2", // shaded
+			"com.fasterxml.jackson.core:jackson-core:2.20.0",
+			"tools.jackson.core:jackson-databind:3.0.0",
 			"org.apache.httpcomponents.client5:httpclient5:5.4.2",
 			"fr.inria.gforge.spoon:spoon-core:11.2.0",
 			"commons-logging:commons-logging:1.3.5",
-			"org.springframework:spring-web:6.2.2",
-			"com.h2database:h2:2.3.232",
 			"org.hamcrest:hamcrest:3.0",
-			"org.springframework:spring-beans:6.2.2",
 			"org.osgi:org.osgi.core:6.0.0",
 			"com.alibaba:fastjson:2.0.54",
 			"commons-collections:commons-collections:3.2.2",
 			"org.json:json:20250107",
 			"commons-beanutils:commons-beanutils:1.10.0",
-			"com.squareup.retrofit2:retrofit:2.11.0"
+			"org.apache.maven:maven-plugin-api:3.9.11",
+			"org.ow2.asm:asm:9.9",
+			"com.google.auto.service:auto-service:1.1.1",
+			"org.mapstruct:mapstruct:1.6.3",
+			"io.reactivex.rxjava3:rxjava:3.1.12",
+			"org.openjdk.jmh:jmh-core:1.37",
+			"io.micrometer:micrometer-core:1.15.5",
+			"org.glassfish.jersey.core:jersey-server:3.1.11",
+			"org.glassfish.jersey.core:jersey-client:3.1.11"
+			//"org.hibernate.orm:hibernate-core:7.1.4.Final", // Missing dependencies
+			//"io.vertx:vertx-core:5.0.4", // Missing dependencies
+			//"org.quartz-scheduler:quartz:2.5.0", // jakarta/JBoss missing
+			//"org.immutables:value:2.11.6", // Missing dependencies
+			//"org.mockito:mockito-core:5.20.0", // Missing dependencies
+			//"org.projectlombok:lombok:1.18.42", // Missing dependencies
+			//"com.h2database:h2:2.3.232", // javax
+			//"org.springframework:spring-core:6.1.5", // Missing dependencies
+			//"org.springframework:spring-context:6.2.12", // Missing dependencies
+			//"org.springframework:spring-web:6.2.2", // Missing dependencies
+			//"io.projectreactor:reactor-core:3.6.3", // Missing dependencies
+			//"org.apache.kafka:kafka-clients:4.1.0", // Missing dependencies
+			//"org.junit.jupiter:junit-jupiter-api:6.0.0", // Java > 21
 		);
 	}
 
 	@ParameterizedTest(name = "{0}")
 	@MethodSource("libraries")
-	@Timeout(value = 2, unit = TimeUnit.MINUTES)
+	@Timeout(value = 3, unit = TimeUnit.MINUTES)
 	void analyzeLibrary(String libraryGAV) throws Exception {
 		var parts = libraryGAV.split(":");
 		var groupId = parts[0];
@@ -107,12 +133,6 @@ class PopularLibrariesTestIT {
 			.classpath(classpath)
 			.extractorType(ExtractorType.ASM)
 			.build();
-		var spoonLibrary = Library
-			.builder()
-			.location(sourcesDir)
-			.classpath(classpath)
-			.extractorType(ExtractorType.SPOON)
-			.build();
 		var jdtLibrary = Library
 			.builder()
 			.location(sourcesDir)
@@ -130,37 +150,42 @@ class PopularLibrariesTestIT {
 		var jdtApi = Roseau.buildAPI(jdtLibrary);
 		long jdtApiTime = sw.elapsed().toMillis();
 
-		// Spoon API
-		sw.reset().start();
-		var spoonApi = Roseau.buildAPI(spoonLibrary);
-		long spoonApiTime = sw.elapsed().toMillis();
+		var asmVisitor = new ReferenceVisitor();
+		asmVisitor.$(asmApi).visit();
+		var jdtVisitor = new ReferenceVisitor();
+		jdtVisitor.$(jdtApi).visit();
+
+		if (asmVisitor.unresolved.size() + jdtVisitor.unresolved.size() > 0) {
+			System.out.println("classpath=" + classpath);
+			System.out.printf("Unresolved references: asm=%d jdt=%d%n",
+				asmVisitor.unresolved.size(), jdtVisitor.unresolved.size());
+			fail("Unresolved references");
+		}
 
 		// Diffs
-		var asmToJdtBCs = Roseau.diff(asmApi, jdtApi).getAllBreakingChanges();
-		var jdtToAsmBCs = Roseau.diff(jdtApi, asmApi).getAllBreakingChanges();
-		var spoonToSpoonBCs = Roseau.diff(spoonApi, spoonApi).getAllBreakingChanges();
-		var jdtToJdtBCs = Roseau.diff(jdtApi, jdtApi).getAllBreakingChanges();
 		sw.reset().start();
 		var asmToAsmBCs = Roseau.diff(asmApi, asmApi).getAllBreakingChanges();
 		long diffTime = sw.elapsed().toMillis();
+		var jdtToJdtBCs = Roseau.diff(jdtApi, jdtApi).getAllBreakingChanges();
+		var asmToJdtBCs = Roseau.diff(asmApi, jdtApi).getAllBreakingChanges();
+		var jdtToAsmBCs = Roseau.diff(jdtApi, asmApi).getAllBreakingChanges();
 
 		// Stats
 		long loc = countLinesOfCode(sourcesDir);
-		int numTypes = spoonApi.getLibraryTypes().getAllTypes().size();
-		int numMethods = spoonApi.getLibraryTypes().getAllTypes().stream()
+		int numTypes = jdtApi.getLibraryTypes().getAllTypes().size();
+		int numMethods = jdtApi.getLibraryTypes().getAllTypes().stream()
 			.mapToInt(type -> type.getDeclaredMethods().size())
 			.sum();
-		int numFields = spoonApi.getLibraryTypes().getAllTypes().stream()
+		int numFields = jdtApi.getLibraryTypes().getAllTypes().stream()
 			.mapToInt(type -> type.getDeclaredFields().size())
 			.sum();
 
 		System.out.printf("Processed %s (%d LoC, %d types, %d methods, %d fields)%n" +
-				"\tSpoon: %dms%n" +
 				"\tASM: %dms; %dms diff%n" +
 				"\tJDT: %dms%n" +
 				"\tBCs: %s%n",
 			libraryGAV, loc, numTypes, numMethods, numFields,
-			spoonApiTime, asmApiTime, diffTime, jdtApiTime,
+			asmApiTime, diffTime, jdtApiTime,
 			asmToAsmBCs.size());
 
 		if (!jdtToAsmBCs.isEmpty() || !asmToJdtBCs.isEmpty()) {
@@ -173,12 +198,14 @@ class PopularLibrariesTestIT {
 		}
 
 		// Check everything went well
-		assertFalse(spoonApi.getLibraryTypes().getAllTypes().stream().findAny().isEmpty());
-		assertFalse(asmApi.getLibraryTypes().getAllTypes().stream().findAny().isEmpty());
-		assertFalse(jdtApi.getLibraryTypes().getAllTypes().stream().findAny().isEmpty());
-		assertEquals(0, spoonToSpoonBCs.size());
-		assertEquals(0, jdtToJdtBCs.size());
-		assertEquals(0, asmToAsmBCs.size());
+		assertThat(asmApi.getLibraryTypes().getAllTypes()).isNotEmpty();
+		assertThat(jdtApi.getLibraryTypes().getAllTypes()).isNotEmpty();
+		assertThat(jdtToJdtBCs).isEmpty();
+		assertThat(asmToAsmBCs).isEmpty();
+
+		// We don't really want to fail on those for now
+		assumeThat(jdtToAsmBCs).isEmpty();
+		assumeThat(asmToJdtBCs).isEmpty();
 	}
 
 	private static Path downloadSourcesJar(String groupId, String artifactId, String version) {
@@ -255,6 +282,25 @@ class PopularLibrariesTestIT {
 				.sum();
 		} catch (IOException ignored) {
 			return -1L;
+		}
+	}
+
+	static class ReferenceVisitor extends AbstractAPIVisitor {
+		API api;
+		Set<TypeReference<?>> unresolved = new HashSet<>();
+
+		@Override
+		public Visit $(API api) {
+			this.api = api;
+			return super.$(api);
+		}
+
+		@Override
+		public <U extends TypeDecl> Visit typeReference(TypeReference<U> it) {
+			if (api.resolver().resolve(it).isEmpty()) {
+				unresolved.add(it);
+			}
+			return super.typeReference(it);
 		}
 	}
 }
