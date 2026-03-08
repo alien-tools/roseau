b28c33747b0c6df77c3d1b2024111c40a8286f74
Thomas Degueule <thomas.degueule@labri.fr>
2026-03-08 13:49:08 +0100

    Distinguish source-breaking METHOD_OVERRIDABLE_NOW_STATIC from binary-breaking METHOD_NOW_STATIC

--- core/src/main/java/io/github/alien/roseau/diff/rules/breaking/MethodNowStatic.java
+++ core/src/main/java/io/github/alien/roseau/diff/rules/breaking/MethodNowStatic.java
@@ -10,6 +10,10 @@ public class MethodNowStatic implements MemberRule<MethodDecl> {
 	public void onMatched(MethodDecl oldMethod, MethodDecl newMethod, MemberRuleContext ctx) {
 		if (!oldMethod.isStatic() && newMethod.isStatic()) {
 			ctx.builder().memberBC(BreakingChangeKind.METHOD_NOW_STATIC, ctx.oldType(), oldMethod, newMethod);
+
+			if (!ctx.v1().isEffectivelyFinal(ctx.oldType(), oldMethod)) {
+				ctx.builder().memberBC(BreakingChangeKind.METHOD_OVERRIDABLE_NOW_STATIC, ctx.oldType(), oldMethod, newMethod);
+			}
 		}
 	}
 }
